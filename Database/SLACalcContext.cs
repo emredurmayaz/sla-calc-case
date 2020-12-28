00894154476989711519a614ebd39d4e0a0e904e
Emre Durmayaz <emredurmayaz@hotmail.com>
2020-12-28 14:49:52 +0300

    Add project files.

---
+++ Database/SLACalcContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SLACalculation.Database
+{
+    public class SLACalcContext : DbContext
+    {
+        public SLACalcContext(DbContextOptions<SLACalcContext> options)
+            : base(options)
+        {
+
+        }
+
+        //public virtual DbSet<Test> Test { get; set; }
+    }
+}
